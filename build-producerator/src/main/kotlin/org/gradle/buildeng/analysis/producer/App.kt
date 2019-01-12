3bfbe54ba6bc19f3100811d817027687ad584cab
Eric Wendelin <eric@gradle.com>
2019-01-11 20:18:07 -0700

    Make number of days to backfill an env var

--- build-producerator/src/main/kotlin/org/gradle/buildeng/analysis/producer/App.kt
+++ build-producerator/src/main/kotlin/org/gradle/buildeng/analysis/producer/App.kt
@@ -5,8 +5,9 @@ import java.time.Duration
 import java.time.Instant
 
 fun main(args: Array<String>) {
-    // TODO: change this to days
-    val since = Instant.now().minus(Duration.ofMinutes(5))
+    val since = Instant.now().minus(
+            Duration.ofDays(System.getenv("BACKFILL_DAYS").toLong())
+    )
     BuildProducer(ServerConnectionInfo.fromEnv())
             .produceFrom(
                     System.getenv("PUBSUB_TOPIC"),
