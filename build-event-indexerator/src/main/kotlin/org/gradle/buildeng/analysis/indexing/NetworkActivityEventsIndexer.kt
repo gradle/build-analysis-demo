db16fdc7204685e91465cc7a8e1dcaf7f4b6450e
Eric Wendelin <eric@gradle.com>
2019-01-30 15:44:15 -0700

    Spike indexing NetworkDownloadActivity events

---
+++ build-event-indexerator/src/main/kotlin/org/gradle/buildeng/analysis/indexing/NetworkActivityEventsIndexer.kt
@@ -0,0 +1,28 @@
+package org.gradle.buildeng.analysis.indexing
+
+import com.google.api.services.bigquery.model.TableSchema
+import com.google.api.services.bigquery.model.TimePartitioning
+import org.gradle.buildeng.analysis.model.BigQueryTableSchemaGenerator
+import org.gradle.buildeng.analysis.model.DependencyResolution
+import org.gradle.buildeng.analysis.model.NetworkActivity
+import org.gradle.buildeng.analysis.transform.NetworkActivityEventsJsonTransformer
+
+
+object NetworkActivityEventsIndexer {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val tableSchema = TableSchema()
+                .setFields(BigQueryTableSchemaGenerator.generateFieldList(DependencyResolution::class))
+        val timePartitioning = TimePartitioning().setField(NetworkActivity::startTimestamp.name)
+
+        val (pipe, options) = KPipe.from<IndexingDataflowPipelineOptions>(args)
+
+        pipe.fromFiles(input = options.input)
+                .filter { it.value.contains("\"eventType\":\"NetworkDownloadActivity") }
+                .flatMap { NetworkActivityEventsJsonTransformer().transform(it.value) }
+                .map { convertJsonToTableRow(it)!! }
+                .toTable("Write to BigQuery", options.output, tableSchema, timePartitioning)
+
+        pipe.run().waitUntilFinish()
+    }
+}
